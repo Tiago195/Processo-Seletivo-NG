bc42fe14ece30c0b2640295cb5169ea754b01742
Tiago195 <tiagocf475@gmail.com>
2022-11-15 19:53:15 -0300

    initial commit

---
+++ backend/prisma/migrations/20221115224614_init/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" SERIAL NOT NULL,
+    "balance" INTEGER NOT NULL DEFAULT 100,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "accountId" INTEGER NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "debitedAccountId" INTEGER NOT NULL,
+    "creditedAccountId" INTEGER NOT NULL,
+    "value" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_accountId_key" ON "User"("accountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Transaction_debitedAccountId_key" ON "Transaction"("debitedAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Transaction_creditedAccountId_key" ON "Transaction"("creditedAccountId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "debited_acount_fk" FOREIGN KEY ("debitedAccountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "credited_acount_fk" FOREIGN KEY ("debitedAccountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ backend/src/server.ts
@@ -0,0 +1,5 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3001, () => console.log("init port 3001"));
