7aac6857ed5961da0ac8ccef68e991ae679a2784
Tiago195 <tiagocf475@gmail.com>
2022-11-16 16:01:06 -0300

    :construction: chore: update docker-compose and package.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,16 @@
 import express from "express";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { userRouter } from "./routes/user.routes";
 
 const app = express();
+app.use(express.json());
 
+app.use("/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).end("OK")
+})
+
+
+app.use(errorHandler);
 app.listen(3001, () => console.log("init port 3001"));
