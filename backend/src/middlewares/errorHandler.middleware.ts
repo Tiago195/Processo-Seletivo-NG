3f96626360dd444d219d976d8c562ed6a94e0bda
Tiago195 <tiagocf475@gmail.com>
2022-11-16 15:58:17 -0300

    feat: middleware error

---
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express"
+import { StatusCodes } from "http-status-codes";
+import { ErrorApi } from "../utils/errorGenerate";
+
+const errorHandler = (er: ErrorApi, req: Request, res: Response, next: NextFunction) => {
+  if (er.statusCode) return res.status(er.statusCode).json({ message: er.message });
+
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: er.message });
+}
+
+export { errorHandler }
---
+++ backend/src/utils/errorGenerate.ts
@@ -0,0 +1,10 @@
+class ErrorApi extends Error {
+  public statusCode: number
+
+  constructor(msg: string, statusCode: number) {
+    super(msg)
+    this.statusCode = statusCode
+  }
+}
+
+export { ErrorApi }
