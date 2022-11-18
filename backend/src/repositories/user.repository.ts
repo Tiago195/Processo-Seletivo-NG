f0df147b8d7d2024a8fc3e1c9f4ebf4b753172be
Tiago195 <tiagocf475@gmail.com>
2022-11-18 12:35:46 -0300

    refactor: :recycle: user repository

--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -1,23 +1,12 @@
 import { User } from '@prisma/client';
-import by from 'bcrypt';
-import { Token } from '../utils/jwt';
 import { db } from '../db';
 
 export class UserRepository {
-  private encodePassword (password: string): string {
-    const salt = by.genSaltSync(5);
-    const hash = by.hashSync(password, salt);
-
-    return hash;
-  }
-
   public async create (user: User) {
-    const hash = this.encodePassword(user.password);
-
-    const { password, ...newUser } = await db.user.create({
+    const { password, ...userWithoutPassword } = await db.user.create({
       data: {
         username: user.username,
-        password: hash,
+        password: user.password,
         account: {
           create: {
             balance: 100
@@ -26,7 +15,7 @@ export class UserRepository {
       }
     });
 
-    return { ...newUser, token: Token.encodeToken(newUser) };
+    return userWithoutPassword;
   }
 
   public async getByUserName (username: string) {
