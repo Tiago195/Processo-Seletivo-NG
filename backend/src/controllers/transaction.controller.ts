11c5aece1b75f243b6cd132d42c2e38a742ac33d
Tiago195 <tiagocf475@gmail.com>
2022-11-18 15:11:09 -0300

    feat: :sparkles: transaction controller create

--- backend/src/controllers/transaction.controller.ts
+++ backend/src/controllers/transaction.controller.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { transactionService, TransactionService } from '../services/transaction.service';
 
 export class TransactionController {
-  public async create (req: Request, res: Response) { }
+  private readonly _service: TransactionService;
+
+  constructor (service: TransactionService) {
+    this._service = service;
+    this.create = this.create.bind(this);
+    this.getAll = this.getAll.bind(this);
+  }
+
+  public async create (req: Request, res: Response) {
+    const transaction = await this._service.create(req.body);
+
+    res.status(StatusCodes.CREATED).json(transaction);
+  }
 
   public async getAll (req: Request, res: Response) { }
 }
+
+export const transactionController = new TransactionController(transactionService);
