09df41982e06bbf021cd3b70dc7750ddbf5406c7
Tiago195 <tiagocf475@gmail.com>
2022-11-18 19:09:36 -0300

    feat: :sparkles: transaction repository getall with filters

--- backend/src/repositories/transaction.repository.ts
+++ backend/src/repositories/transaction.repository.ts
@@ -1,6 +1,6 @@
 // import { User, Transaction, Account } from '@prisma/client';
 import { db } from '../db';
-import { newTrasaction } from '../interfaces/ITransaction.interface';
+import { IQueryFilter } from '../interfaces/ITransaction.interface';
 
 export class TransactionRepository {
   private cashIn (accountId: number, balance: number) {
@@ -25,27 +25,49 @@ export class TransactionRepository {
     });
   }
 
-  private createTransaction (transaction: newTrasaction) {
-    return db.transaction.create({
-      data: {
-        value: transaction.value,
-        debitedAccountId: transaction.debitedAccountId,
-        creditedAccountId: transaction.creditedAccountId
+  private createTransaction (debitedAccountId: number, creditedAccountId: number, value: number) {
+    return db.transaction.create({ data: { value, debitedAccountId, creditedAccountId } });
+  }
+
+  public async create (debitedAccountId: number, creditedAccountId: number, value: number) {
+    return (await db.$transaction([
+      this.createTransaction(debitedAccountId, creditedAccountId, value),
+      this.cashIn(creditedAccountId, value),
+      this.cashOut(debitedAccountId, value)
+    ]))[0];
+  }
+
+  public async getUserById (username: string) {
+    return await db.user.findUnique({
+      where: { username },
+      select: {
+        username: true,
+        account: true,
+        id: true
       }
     });
   }
 
-  public async create (transaction: newTrasaction) {
-    return (await db.$transaction([
-      this.createTransaction(transaction),
-      this.cashIn(transaction.creditedAccountId, transaction.value),
-      this.cashOut(transaction.debitedAccountId, transaction.value)
-    ]))[0];
-  }
+  public async getAll (query: IQueryFilter, id: number) {
+    const gte = new Date(query.data ?? '2022-01-01');
+    const lte = new Date(query.data ?? Date.now());
 
-  public async getAccountById (id: number) {
-    return await db.account.findUnique({
-      where: { id }
+    if (query.data) lte.setDate(gte.getDate() + 1);
+
+    return await db.transaction.findMany({
+      where: {
+        createdAt: { gte, lte },
+        creditedAccountId: query.in ? id : undefined,
+        debitedAccountId: query.out ? id : undefined,
+        OR: [
+          {
+            creditedAccountId: id
+          },
+          {
+            debitedAccountId: id
+          }
+        ]
+      }
     });
   }
 }
