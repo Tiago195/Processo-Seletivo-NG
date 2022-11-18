13919a08045ab2b7a001fd97c2b10da9e9c8b2e1
Tiago195 <tiagocf475@gmail.com>
2022-11-18 19:10:27 -0300

    refactor: :recycle: change body request

--- backend/src/services/transaction.service.ts
+++ backend/src/services/transaction.service.ts
@@ -1,5 +1,5 @@
 import { StatusCodes } from 'http-status-codes';
-import { newTrasaction } from '../interfaces/ITransaction.interface';
+import { IQueryFilter, newTrasaction } from '../interfaces/ITransaction.interface';
 import { transactionRepository, TransactionRepository } from '../repositories/transaction.repository';
 import { ErrorApi } from '../utils/errorGenerate';
 
@@ -10,15 +10,26 @@ export class TransactionService {
     this._repository = repository;
   }
 
-  public async create (transaction: newTrasaction) {
-    const existCreditedAccountId = await this._repository.getAccountById(transaction.creditedAccountId);
-    const existDebitedAccountId = await this._repository.getAccountById(transaction.debitedAccountId);
+  public async create (transaction: newTrasaction, username: string) {
+    const existCreditedAccountId = await this._repository.getUserById(transaction.username);
+    const existDebitedAccountId = await this._repository.getUserById(username);
 
-    if (!existCreditedAccountId || !existDebitedAccountId) throw new ErrorApi('Credited or Debited account invalid', StatusCodes.BAD_REQUEST);
+    if (transaction.username === username) throw new ErrorApi('Unathorized transaction', StatusCodes.UNAUTHORIZED);
 
-    if (existDebitedAccountId.balance - transaction.value < 0) throw new ErrorApi('Value invalid', StatusCodes.BAD_REQUEST);
+    if (!existCreditedAccountId) throw new ErrorApi('User not found', StatusCodes.NOT_FOUND);
 
-    return await this._repository.create(transaction);
+    if (existDebitedAccountId!.account.balance - transaction.value < 0) throw new ErrorApi('insufficient balance', StatusCodes.BAD_REQUEST);
+
+    const response = {
+      transaction: await this._repository.create(existDebitedAccountId!.id, existCreditedAccountId.id, transaction.value),
+      balance: existDebitedAccountId!.account.balance - transaction.value
+    };
+
+    return response;
+  }
+
+  public async getAll (query: IQueryFilter, id: number) {
+    return await this._repository.getAll(query, id);
   }
 }
 
