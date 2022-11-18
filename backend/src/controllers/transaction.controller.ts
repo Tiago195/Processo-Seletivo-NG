37de45b7aa75f2d3b799222d9e838839aa50353f
Tiago195 <tiagocf475@gmail.com>
2022-11-18 19:14:36 -0300

    feat: :sparkles: transaction controller getall

--- backend/src/controllers/transaction.controller.ts
+++ backend/src/controllers/transaction.controller.ts
@@ -1,6 +1,9 @@
+import { User } from '@prisma/client';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { newTrasaction } from '../interfaces/ITransaction.interface';
 import { transactionService, TransactionService } from '../services/transaction.service';
+import { Token } from '../utils/jwt';
 
 export class TransactionController {
   private readonly _service: TransactionService;
@@ -12,12 +15,20 @@ export class TransactionController {
   }
 
   public async create (req: Request, res: Response) {
-    const transaction = await this._service.create(req.body);
+    const { username } = Token.decodeToken(req.headers.authorization as string) as User;
+
+    const transaction = await this._service.create(req.body, username);
 
     res.status(StatusCodes.CREATED).json(transaction);
   }
 
-  public async getAll (req: Request, res: Response) { }
+  public async getAll (req: Request, res: Response) {
+    const { id } = Token.decodeToken(req.headers.authorization as string) as User;
+
+    const transactions = await this._service.getAll(req.query, id);
+
+    res.status(StatusCodes.OK).json(transactions);
+  }
 }
 
 export const transactionController = new TransactionController(transactionService);
