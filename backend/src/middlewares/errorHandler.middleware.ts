442742d08c81152b50822ad31abc91a076bb901d
Tiago195 <tiagocf475@gmail.com>
2022-11-21 12:22:06 -0300

    refactor: :recycle: change error message 500

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -5,7 +5,7 @@ import { ErrorApi } from '../utils/errorGenerate';
 const errorHandler = (er: ErrorApi, _req: Request, res: Response, _next: NextFunction) => {
   if (er.statusCode) return res.status(er.statusCode).json({ message: er.message });
 
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: er.message });
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
 };
 
 export { errorHandler };
